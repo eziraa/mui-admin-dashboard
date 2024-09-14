1db6f7157a4b97622cea79539fdd399de85c8ac4
eziraa <ezratgab@gmail.com>
2024-09-14 22:45:51 +0300

    configurring the roor app component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { ColorModeContext, useMode } from "./theme";
+import TopBar from "./scenes/globals/TopBar";
+
 function App() {
-  return <div className="app"></div>;
+  const [theme, colorMode] = useMode();
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <main className="content">
+            <TopBar />
+          </main>
+        </div>
+        {/* Your app components */}
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
 }
 
 export default App;
