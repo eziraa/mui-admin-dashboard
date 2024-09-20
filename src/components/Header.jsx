f854df1d1cba2745a3c88d8941fab30896ea0e08
sutual <ezirayallew@gmail.com>
2024-09-20 21:22:05 +0300

    updating the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import { Box, Typography, useTheme } from "@mui/material";
+import { Typography, Box, useTheme } from "@mui/material";
 import { tokens } from "../theme";
 
 const Header = ({ title, subtitle }) => {
@@ -10,9 +10,7 @@ const Header = ({ title, subtitle }) => {
         variant="h2"
         color={colors.grey[100]}
         fontWeight="bold"
-        sx={{
-          mb: "5px",
-        }}
+        sx={{ m: "0 0 5px 0" }}
       >
         {title}
       </Typography>
